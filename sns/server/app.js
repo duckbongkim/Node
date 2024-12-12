ddae3f74e9e46614c47c9fe0ad80264a3b9e8405
choidongjin <dongjin1508@gmail.com>
2024-12-12 21:39:39 +0900

    snsapp

--- sns/server/app.js
+++ sns/server/app.js
@@ -61,7 +61,7 @@ app.use((req,res,next)=>{
 
 app.use((err,req,res,next)=>{
     console.error(err)
-    res.status(err.status || 500).json({error:message})
+    res.status(err.status || 500).json({error:err.message})
 })
 
 
